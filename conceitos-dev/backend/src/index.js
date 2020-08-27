34a899dd659288896492208665602a4aefd5c213
Matheus Acácio <matheusacacio@unigranrio.br>
2020-08-27 17:24:05 -0300

    comentando projeto

--- conceitos-dev/backend/src/index.js
+++ conceitos-dev/backend/src/index.js
@@ -1,9 +1,15 @@
+// Reqisitando as funções do pacote do express
 const express = require('express');
 
+// Invocando todos os métodos do objeto do express em uma variável
 const app = express();
 
+// Gerando a primeira requisição HTTP, enviando uma resposta para o cliente saber que a conexão esta online
 app.get('/', (request, response) => {
     return response.json([{ message: 'Hello World' }]);
 });
 
-app.listen(3333);
+// Porta onde esta sendo ouvida o servidor e uma mensagem no backend para saber que o server esta online
+app.listen(3333, () => {
+    console.log('📡Backend online!');
+});
