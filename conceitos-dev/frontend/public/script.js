5a29cf8f262830b5ef5118d098c35f0f5e4affd1
Matheus Acácio <matheusacacio@unigranrio.br>
2020-09-10 19:09:34 -0300

    Iniciando frontend

---
+++ conceitos-dev/frontend/public/script.js
@@ -0,0 +1,6 @@
+/**
+ * Babel: Converter código do React para um codigo que o browser entenda.
+ * Webpack: Para cada tipo de arquivo (.js, .css, .png) o webpack converte o código dwe maneira diferente.
+ * 
+ * Loaders: babelç-loader, css-loader, image-loader
+ */
